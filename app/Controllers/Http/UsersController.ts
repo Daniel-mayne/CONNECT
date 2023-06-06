ec477a97cda7cf916172a29fbf9c21995d2b5a3e
Daniel Bento <danielmayne2021@gmail.com>
2023-06-06 18:04:56 -0300

    Criando UsersController e seus metodos

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,13 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import StoreValidator from 'App/Validators/User/StoreValidator'
+import UpdateValidator from 'App/Validators/User/UpdateValidator'
 
 export default class UsersController {
-  public async index({}: HttpContextContract) {}
+  public async index({ auth }: HttpContextContract) {
+    const user = await User.query().where('id', auth.user!.id)
+    return user
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = await request!.validate(StoreValidator)
+    const user = await User.create(data)
+    return user
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params, auth }: HttpContextContract) {
+    const user = await User.query().where('id', auth.user!.id).where('id', params.id).firstOrFail()
+    return user
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, params, auth }: HttpContextContract) {
+    const user = await User.query().where('id', auth.user!.id).where('id', params.id).firstOrFail()
+    const data = await request!.validate(UpdateValidator)
+    user.merge(data)
+    return user
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ params, auth}: HttpContextContract) { 
+    const user = await User.query().where('id', auth.user!.id).where('id', params.id).firstOrFail()
+    await user.delete()
+    return "Deletado com sucesso"
+  }
 }
