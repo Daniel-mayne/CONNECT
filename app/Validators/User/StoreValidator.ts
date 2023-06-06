01d05a5b5b9f8ea92280e4ac35f3ae4d88073486
Daniel Bento <danielmayne2021@gmail.com>
2023-06-06 17:31:17 -0300

    Ajustando StoreValidator e UpdateValidator do User

--- app/Validators/User/StoreValidator.ts
+++ app/Validators/User/StoreValidator.ts
@@ -1,11 +1,17 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class StoreValidator {
-  constructor(protected ctx: HttpContextContract) {}
+  constructor(protected ctx: HttpContextContract) { }
 
-  
-  public schema = schema.create({})
+
+  public schema = schema.create({
+    email: schema.string({}, [rules.trim(), rules.email(), rules.required(), rules.unique({ table: 'users', column: 'email' })]),
+    name: schema.string({}, [rules.trim()]),
+    cpf: schema.string({}, [rules.trim(), rules.required(), rules.unique({ table: 'users', column: 'cpf' })]),
+    phone: schema.string({}, [rules.trim()]),
+    password: schema.string({}, [rules.trim()])
+  })
 
 
   public messages: CustomMessages = {}
--- app/Validators/User/UpdateValidator.ts
+++ app/Validators/User/UpdateValidator.ts
@@ -1,11 +1,17 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   
-  public schema = schema.create({})
+  public schema = schema.create({
+    email: schema.string.optional({}, [rules.trim()]),
+    name: schema.string.optional({}, [rules.trim()]),
+    cpf: schema.string.optional({}, [rules.trim()]),
+    phone: schema.string.optional({}, [rules.trim()]),
+    password: schema.string.optional({}, [rules.trim()])
+  })
 
 
   public messages: CustomMessages = {}
